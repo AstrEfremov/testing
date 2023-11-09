7c5dd99c6d3e70341219cfa6c1a9cf92be5a1be9
AstrEfremov <101562414+AstrEfremov@users.noreply.github.com>
2023-11-09 13:36:32 +0400

    Done

--- ex/ex/src/main/java/com/testExercise/ex/ExApplication.java
+++ ex/ex/src/main/java/com/testExercise/ex/ExApplication.java
@@ -3,6 +3,9 @@ package com.testExercise.ex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//ВВедите в адресной строке http://localhost:8080/counts/   -  далее введите любой текст для подсчета
+// Например http://localhost:8080/counts/ansdjaknsdasnkd
 @SpringBootApplication
 public class ExApplication {
 
--- ex/ex/src/main/java/com/testExercise/ex/controller/FirstController.java
+++ ex/ex/src/main/java/com/testExercise/ex/controller/FirstController.java
@@ -1,18 +1,20 @@
 package com.testExercise.ex.controller;
 
 
+import com.testExercise.ex.engine.ResponseChars;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/counts")
 
 public class FirstController {
 
     @ResponseBody
-    @RequestMapping("/sayHello")
-    public String sayHello(){
-        return "/edit";
+    @RequestMapping("/{string}")
+      public String sayHello(@ModelAttribute("string") String say){
+        return ResponseChars.counts(say);
     }
 }
